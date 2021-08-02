690705141519e171392e897e34866edcbbc8c769
Estee Chai <estee.chai@magia-solutions.com>
2021-08-02 10:33:49 +0800

    added routing modules

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,45 @@
+import {NgModule} from "@angular/core";
+import {RecipesComponent} from "./recipes.component";
+import {RecipeListComponent} from "./recipe-list/recipe-list.component";
+import {RecipeDetailComponent} from "./recipe-detail/recipe-detail.component";
+import {RecipeItemComponent} from "./recipe-list/recipe-item/recipe-item.component";
+import {RecipeStartComponent} from "./recipe-start/recipe-start.component";
+import {RecipeEditComponent} from "./recipe-edit/recipe-edit.component";
+import {Router, RouterModule} from "@angular/router";
+import {BrowserModule} from "@angular/platform-browser";
+import {CommonModule} from "@angular/common";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {AuthGuard} from "../auth/auth.guard";
+import {RecipesResolverService} from "./recipes-resolver.service";
+import {RecipesRoutingModule} from "./recipes-routing.module";
+
+@NgModule({
+  declarations:[
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeStartComponent,
+    RecipeEditComponent,
+  ],
+  // import Modules as components declared in this modules can only access to imported modules here.
+  imports:[
+    RouterModule,
+    BrowserModule,
+    CommonModule,
+    ReactiveFormsModule,
+   RecipesRoutingModule,
+  ],
+  // export all declared components so app.module.ts can access to these components.
+  exports:[
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeStartComponent,
+    RecipeEditComponent,
+  ]
+})
+export class RecipesModule {
+
+}
